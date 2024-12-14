fc2a47c2c69e55bb9f8650aff8cbc9ceca7c7c29
yuusha25 <razkyagengsyahputra95@gmail.com>
2024-12-14 17:51:49 +0700

    backend

--- src/routes/authGoogle.js
+++ src/routes/authGoogle.js
@@ -95,7 +95,7 @@ const setupAuthRoutes = (app) => {
 
         // Redirect to home page after successful login or signup
         return res.redirect(
-          `http://localhost:5173/login-success?id=${user.id}`
+          `https://chronocam.vercel.app/login-success?id=${user.id}`
         );
 
         // return res.redirect(`http://localhost:5173/?id=${user.id}`);
