47ce73d572cfd3c99a2ed516502a9f80b9aa1bb5
yuusha25 <razkyagengsyahputra95@gmail.com>
2024-12-14 19:50:34 +0700

    backend

--- src/routes/authGoogle.js
+++ src/routes/authGoogle.js
@@ -10,10 +10,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:[
-          "https://choronocam.vercel.app/auth/google/callback",
-          "http://localhost:8080/auth/google/callback"
-      ],
+      callbackURL: "https://choronocam.vercel.app/auth/google/callback"
+
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
