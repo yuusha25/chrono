68cba598dbe3947655590196932dc394d66049c7
yuusha25 <razkyagengsyahputra95@gmail.com>
2024-12-14 16:29:04 +0700

    backend

--- server.js
+++ server.js
@@ -27,7 +27,10 @@ const app = express();
 // Middleware CORS
 app.use(
   cors({
-    origin: "https://chronocam.vercel.app/",
+    origin: [
+      "https://chronocam.vercel.app",
+      "http://localhost:5173"
+    ],
     methods: ["GET", "POST", "PUT"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
